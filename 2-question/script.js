7b9a73454a1120d57265448e718a42fe54374084
GopinathSM17 <58787612+GopinathSM17@users.noreply.github.com>
2025-02-28 02:01:14 +0530

    left all and right all button disable completed

--- 2-question/script.js
+++ 2-question/script.js
@@ -38,13 +38,20 @@ leftButton.addEventListener('click', (e) => {
     const rightsideLabels = document.querySelector('.right-box').children;
     const arr = Array.from(rightsideLabels);
     arr.forEach(val => {
-        // console.log(val.querySelector('input').checked);
+       
         let checkedItem = val.querySelector('input').checked;
         if (checkedItem) {
             leftSideBox.append(val);
+            rightAllButton.disabled = false;
         }
         val.querySelector('input').checked = false;
     });
+
+    const rightSideLabels = document.querySelector('.right-box').children
+    console.log(rightSideLabels);
+    if (rightSideLabels.length < 1) {
+        leftAllButton.disabled = true;
+    }
 });
 
 
@@ -55,13 +62,22 @@ rightButton.addEventListener('click', (e) => {
     console.log("mouse click right btn");
     const leftsideLabels = document.querySelector('.left-box').children;
     const arr = Array.from(leftsideLabels);
-    arr.forEach(val =>{
+    arr.forEach(val => {
+
         let checkedItem = val.querySelector('input').checked;
-        if(checkedItem) {
+        if (checkedItem) {
             rightSideBox.append(val);
+            leftAllButton.disabled=false;
         }
-        val.querySelector('input').checked= false;
+        val.querySelector('input').checked = false;
     });
+
+    const leftSideLabels = document.querySelector('.left-box').children
+    if (leftSideLabels.length < 1) {
+        rightAllButton.disabled = true;
+    }
+    
+    
 });
 
 
@@ -75,6 +91,7 @@ leftAllButton.addEventListener('click', (e) => {
     arr.forEach((val) => {
         leftSideBox.append(val);
     })
+    rightAllButton.disabled = false;
 });
 
 
@@ -88,6 +105,24 @@ rightAllButton.addEventListener('click', (e) => {
     arr.forEach(val => {
         rightSideBox.append(val);
     });
+    leftAllButton.disabled = false;
 });
 
 
+//right all button disable feature
+rightAllButton.addEventListener('click', (e)=>{
+    const leftSideLabels = document.querySelector('.left-box').children
+    if (leftSideLabels.length < 1) {
+        const rightAllButton = document.querySelector('.right-all-btn')
+        rightAllButton.disabled = true;
+    }
+})
+
+//left all button disable feature
+leftAllButton.addEventListener('click', (e)=>{
+    const rightSideLabels = document.querySelector('.right-box').children
+    if (rightSideLabels.length < 1) {
+        const leftAllButton = document.querySelector('.left-all-btn')
+        leftAllButton.disabled = true;
+    }
+})
