39489e0e84189a300af1b0ca324ed853a8edfa55
GopinathSM17 <58787612+GopinathSM17@users.noreply.github.com>
2025-02-28 15:12:41 +0530

    implemented single source of truth

--- 1-question/script.js
+++ 1-question/script.js
@@ -2,17 +2,38 @@
 
 var item = document.getElementById('item');
 var ouptutDiv = document.getElementById('output')
-console.log(ouptutDiv);
+
+let arrOfObjects = [];
 
 item.addEventListener('keydown', (e)=>{
     if(e.key == 'Enter'){
-        console.log(item.value);
+        // console.log(item.value);
         e.preventDefault();
+
         const value = item.value;
+
+        
         if(value != ''){
-            //creating a new Div
+
+            const obj = { 
+                'name' : value,
+                'isChecked' : false 
+            };
+            arrOfObjects.push(obj);
+            
+        }
+
+
+        ouptutDiv.innerHTML = "";
+
+        arrOfObjects.forEach(element => {
+            
+
+            const valueToBeInserted = element.name;
+
+             //creating a new Div
             const newDiv = document.createElement('div');
-            newDiv.textContent = value;
+            newDiv.textContent = valueToBeInserted;
             newDiv.classList.add('text-div')
 
             // creating a button inside the new div
@@ -20,15 +41,18 @@ item.addEventListener('keydown', (e)=>{
             btnElement.textContent='delete';
             newDiv.appendChild(btnElement);
 
-            btnElement.addEventListener('mousedown',(e)=>{
+             // deleting the Div 
+             btnElement.addEventListener('mousedown',(e)=>{
                 newDiv.remove();
             })
+
             // appending the new div inside the outputdiv as a child div
             ouptutDiv.appendChild(newDiv);
 
             // after the enter the value will be back to null
             item.value = '';
-        }
+
+        });
     }
 });
 
